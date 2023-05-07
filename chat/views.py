63e24f7e177fcce11b017927e50f24a92207a4a0
Krish Agrawal <printf.krish@gmail.com>
2023-05-07 18:08:07 +0530

    Sending Msges From Outside Consumers

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 
 # Create your views here.
 def index(request):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)("chat_lobby",
+                             {
+                                 "type": "chat_message",
+                                 "message": "Someone's here",
+                                 "user": "System"
+                             })
     return render(request, "chat/index.html")
 
 
